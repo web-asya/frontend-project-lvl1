670b033fa045df34842dde625a455595f960fb66
web-asya <asyakalininaa@gmail.com>
2021-07-29 17:38:17 +0300

    add Greetings

---
+++ src/cli.js
@@ -0,0 +1,6 @@
+import readlineSync from 'readline-sync';
+const Greetings = () => {
+const name = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${name}!`);
+};
+export default Greetings;
