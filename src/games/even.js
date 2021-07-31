b266fd8624db6e2a18638db357c4979da0d7d743
web-asya <asyakalininaa@gmail.com>
2021-07-31 23:31:37 +0300

    add even.js

--- src/games/even.js
+++ src/games/even.js
@@ -3,11 +3,12 @@ export const startgame = 'Answer "yes" if the number is even, otherwise answer "
 
 export const Question = () => {
     const num = getRandomInt (1,100);
-    console.log ('Question: ${num}');
+    console.log (`Question: ${num}`);
     return num;
 };
 
 export const GameEven = () => {
+    const num = getRandomInt (1,100);
     const elements = [num,num,num];
     for (let i = 0; i < elements.length, i += 1;) {
         let answer = '';
@@ -15,10 +16,11 @@ export const GameEven = () => {
         console.log('Correct!');
     }
     else if (num % 2 !== 0 && answer === 'yes') {
-        console.log("'yes' is wrong answer ;(. Correct answer was 'no'. Let's try again, ${name}!");
+        console.log(`'yes' is wrong answer ;(. Correct answer was 'no'. Let's try again, ${name}!`);
     }
     else if (num % 2 === 0 && answer === 'no') {
-        console.log ("'no' is wrong answer ;(. Correct answer was 'no'. Let's try again, ${name}!");
+        console.log (`'no' is wrong answer ;(. Correct answer was 'no'. Let's try again, ${name}!`);
     }
   };
+  console.log(`Congratulations, ${name}!`);
 };
