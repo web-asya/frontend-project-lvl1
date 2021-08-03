a1ef8924df9bf6f7463184fc0fef3f9f108ca986
web-asya <asyakalininaa@gmail.com>
2021-08-03 17:54:47 +0300

    add brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,7 +1,4 @@
 #!/usr/bin/env node
-import {
-    condition,
-    Number,
-  } from '../src/games/even.js';
-import Game from '../game_layout.js';
-Game (condition, Number);
+import {condition} from '../src/games/even.js';
+import {Game} from '../game_layout.js';
+Game (condition);
