997211e7416c29dffc1a0f7b4d7f9de83acb5f5c
web-asya <85280628+web-asya@users.noreply.github.com>
2021-08-03 14:04:19 +0300

    Update brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,11 +1,7 @@
 #!/usr/bin/env node
-import Greetings from '../src/cli.js';
 import {
-    Question,
-    startgame,
-    GameEven,
+    condition,
+    Number,
   } from '../src/games/even.js';
-Greetings ();
-console.log (startgame);
-Question();
-GameEven();
+import Game from '../game_layout.js';
+Game (condition, Number);
