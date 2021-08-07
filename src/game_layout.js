3c01e55213a45325ed1d889124a9d7c4b374333a
web-asya <asyakalininaa@gmail.com>
2021-08-07 22:20:37 +0300

    add game

--- src/game_layout.js
+++ src/game_layout.js
@@ -2,13 +2,14 @@
 import readlineSync from 'readline-sync';
 
 
-const AmountRounds = 3;
 
 const Game = (description, GameTask) => {
     console.log('Welcome to the Brain Games!');
     const name = readlineSync.question('May I have your name? ');
     console.log(`Hello, ${name}!`);
     console.log(description);
+
+    const AmountRounds = 3;
 
     for (let i = 0; i < AmountRounds; i += 1) {
       let task = GameTask ();
