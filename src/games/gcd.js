af4b07d720cf44ea33033653038eca8860cfda2a
web-asya <asyakalininaa@gmail.com>
2021-08-10 19:59:12 +0300

    add readme

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -5,7 +5,9 @@ const description = 'Find the greatest common divisor of given numbers.';
 const Gcd = (num1,num2) => {
     num1 = Math.abs(num1);
     num2 = Math.abs(num2);
-    if (num2 > num1) {let temp = num1; num1 = num2; num2 = temp;}
+    if (num2 > num1) {
+        let temp = num1; num1 = num2; num2 = temp;
+    }
     while (true) {
         if (num2 == 0) return num1;
         num1 = num1 % num2;
